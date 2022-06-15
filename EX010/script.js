84c211591673715b64d3920d9fd6377125b0ea71
fernandevcordeiro <107525725+fernandevcordeiro@users.noreply.github.com>
2022-06-15 00:14:56 -0300

    Add files via upload

---
+++ EX010/script.js
@@ -0,0 +1,11 @@
+/*--QUESTÃO 10) Desenvolva via JavaScript a seguinte estrutura de código levando em consideração as seguintes regras:
+a) Uma variável deve ter o nome de sorvete de chocolate;
+b) Se o seu valor for igual a chocolate, deve ser exibido no alerta do navegador a mensagem: Amo sorvete de chocolate;
+c) Se o seu valor for outro, deve exibir a mensagem: Prefiro outros sabores..-->*/
+
+// Utilizando if ternário
+var sorveteDeChocolate = 'baunilha';
+sorveteDeChocolate === 'chocolate'? alert(`Amo sorvete de chocolate!`): alert(`Prefiro outros sabores...`);
+
+
+
