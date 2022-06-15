3b6ddab8da8f1cf13064f1de556078e7d8778c57
fernandevcordeiro <107525725+fernandevcordeiro@users.noreply.github.com>
2022-06-15 00:15:14 -0300

    Add files via upload

---
+++ EX011/script.js
@@ -0,0 +1,19 @@
+/*QUESTÃO 11) Na função JavaScript a seguir, responda quais serão os resultados respectivos:
+function multiplicacao (num1,num2) {
+ let resultado = num1 * num2;
+ return resultado;
+}
+multiplica(4, 7);
+multiplica(5, 6);
+multiplica(3, 3)*/
+
+function multiplicacao (num1,num2) {
+    let resultado = num1 * num2;
+    return alert(`O resultado da multiplicação é: `+ resultado);
+}
+
+multiplicacao(4, 7); // Resultado: 28
+multiplicacao(5, 6); // Resultado: 30
+multiplicacao(3, 3); // Resultado: 9
+
+
